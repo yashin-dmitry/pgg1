b451ac1e53f3e3cccf62124499c0cd7411a4f304
Dmitry <160520396+yashin-dmitry@users.noreply.github.com>
2024-02-28 13:51:36 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,7 @@
 def uppercase_string(input_string):
     """
     Принимает строку и возвращает ее в верхнем регистре.
-
-    :param input_string: входная строка
-    :return: строка в верхнем регистре
+    input_string: входная строка
+    return: строка в верхнем регистре
     """
     return input_string.upper()
